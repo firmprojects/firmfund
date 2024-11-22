7c380f819614ea0972babc018646fa631484a8a9
afripoint <devteam@afripointgroup.com>
2024-11-22 19:18:33 +0100

    cmmt

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -54,8 +54,8 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        LeftArrow: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        RightArrow: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        PrevIcon: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
+        NextIcon: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
     />
