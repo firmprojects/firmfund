a82da2b6319adc1ce225dd197b362d9f7dfbe079
afripoint <devteam@afripointgroup.com>
2024-11-22 18:54:52 +0100

    Fix netlify build issue

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -54,8 +54,8 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        LeftArrow: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
+        RightArrow: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
     />
